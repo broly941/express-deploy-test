b8b0afa38707baa547257a87c0c3daaf0dd95e9d
Ilya Korzhavin <ilia.korzhavin@kitrum.com>
2021-11-20 16:34:14 +0300

    Fix errors

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express_1 = require("express");
-var app = (0, express_1["default"])();
-var PORT = process.env.PORT || 80;
-app.listen(PORT, function () {
-    console.log("Server has been started...");
-});
