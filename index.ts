80888bc07691c307eb007829a1edf982f09245f9
Ilya Korzhavin <ilia.korzhavin@kitrum.com>
2021-11-20 17:14:57 +0300

    Fix 404

--- index.ts
+++ index.ts
@@ -1,22 +1,18 @@
 import * as dotenv from "dotenv";
+
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
-import logger from "./middleware/logger";
-
-dotenv.config();
+import logger from "./src/middleware/logger";
 
 const app = express();
+dotenv.config();
 const PORT = process.env.PORT || 80;
 
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(logger);
-
-app.get("/about", (req, res) => {
-  res.send("Heroku express practice");
-});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
